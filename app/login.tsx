5f060cded6fe8e0549bea62f1dc8e8112447907a
caiovini100 <caiomqnvinicius100@gmail.com>
2026-03-31 18:12:35 -0300

    atualização na estrutura

---
+++ app/login.tsx
@@ -0,0 +1,12 @@
+import  React from 'react';
+import { KeyboardAvoidingView, View } from 'react-native';
+
+export default function App() {
+  return(
+    <KeyboardAvoidingView>
+      <view>
+
+      </view>
+      </KeyboardAvoidingView>
+  );
+}
